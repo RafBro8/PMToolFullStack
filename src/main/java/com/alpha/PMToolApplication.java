5ab62f58a3e41b4c156b23e5be33f3a02ff438f9
rbrod <rbrod@allstate.com>
2019-01-09 18:56:45 -0600

    create Entity and Repository plus app file for H2 DB settings

--- src/main/java/com/alpha/PMToolApplication.java
+++ src/main/java/com/alpha/PMToolApplication.java
@@ -1,4 +1,15 @@
 package com.alpha;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.alpha"})
 public class PMToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PMToolApplication.class, args);
+    }
 }
---
+++ src/main/java/com/alpha/repository/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.alpha.repository;
+
+import com.alpha.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}

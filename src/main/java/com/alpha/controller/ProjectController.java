2b9f3ffbb72928070304b7b55050c159d375ce59
rbrod <rbrod@allstate.com>
2019-01-15 18:23:04 -0600

    refactor ProjectController to move validation logic to ValidationErrorService

--- src/main/java/com/alpha/controller/ProjectController.java
+++ src/main/java/com/alpha/controller/ProjectController.java
@@ -2,19 +2,17 @@ package com.alpha.controller;
 
 import com.alpha.domain.Project;
 import com.alpha.service.ProjectService;
+import com.alpha.service.ValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -23,19 +21,17 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private ValidationErrorService validationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if (result.hasErrors()) {
-
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError error: result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-
-            }
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = validationErrorService.mapValidationService(result);
+        if (errorMap != null) {
+            return errorMap;
         }
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
@@ -96,7 +92,6 @@ public class ProjectController {
 //    }
 //}
 //with Java Generics it looks like you can return different types inside same code block
-
 
 //for validation - to return key/value format like - "field":"errorMessage"
 //            Map<String, String> errorMap = new HashMap<>();
